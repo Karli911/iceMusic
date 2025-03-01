e7e49fb806945d290cfb73768c2143cf1c4ff70b
Karli911 <35976946+Karli911@users.noreply.github.com>
2025-03-01 10:16:22 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,7 +27,7 @@ del sys.path[0]
 
 
 class Config:
-    BOT_TOKEN = os.environ["DISCORD_TOKEN"]
+    BOT_TOKEN = os.environ["DISCORD_TOKEN", "MISSING_KEY"]
     SPOTIFY_ID = ""
     SPOTIFY_SECRET = ""
 
